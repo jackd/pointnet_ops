7a9d0bdff1c6f7a0b20adc0bbbda65df23484a89
Dominic Jack <thedomjack@gmail.com>
2019-03-15 13:36:17 +1000

    added back gpu test

--- pointnet/python/ops/group_ops_test.py
+++ pointnet/python/ops/group_ops_test.py
@@ -11,12 +11,13 @@ class GroupPointTest(tf.test.TestCase):
 
     def _test_grad(self, device):
         with tf.device(device):
-            points = tf.constant(np.random.random((1,128,16)).astype('float32'))
-            xyz1 = tf.constant(np.random.random((1,128,3)).astype('float32'))
-            xyz2 = tf.constant(np.random.random((1,8,3)).astype('float32'))
+            points = tf.constant(
+                np.random.random((1,128,16)).astype(np.float32))
+            xyz1 = tf.constant(np.random.random((1,128,3)).astype(np.float32))
+            xyz2 = tf.constant(np.random.random((1,8,3)).astype(np.float32))
             radius = 0.3
             nsample = 32
-            idx, pts_cnt = query_ball_point(radius, nsample, xyz1, xyz2)
+            idx, _ = query_ball_point(radius, nsample, xyz1, xyz2)
             grouped_points = group_point(points, idx)
 
         with self.test_session():
@@ -24,8 +25,8 @@ class GroupPointTest(tf.test.TestCase):
                 points, (1,128,16), grouped_points, (1,8,32,16))
         self.assertLess(err, 1e-4)
 
-    # def test_grad_gpu(self):
-    #     self._test_grad('/gpu:0')
+    def test_grad_gpu(self):
+        self._test_grad('/gpu:0')
 
     def test_grad_cpu(self):
         self._test_grad('/cpu:0')
